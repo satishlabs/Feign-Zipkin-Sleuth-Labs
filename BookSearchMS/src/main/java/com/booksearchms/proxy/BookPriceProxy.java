97ef44a4964fea8fbd4bd5f1b846069e481f1d58
Satish Prasad <spd61185@gmail.com>
2021-01-22 13:40:14 +0530

    BookSearchMS

---
+++ BookSearchMS/src/main/java/com/booksearchms/proxy/BookPriceProxy.java
@@ -0,0 +1,11 @@
+package com.booksearchms.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "MyBookPriceMS")
+public interface BookPriceProxy {
+	
+	@GetMapping("/bookPrice")
+	public String getBookPrice();
+}
